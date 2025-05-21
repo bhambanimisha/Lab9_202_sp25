836d8fa7cfb4bfcaa074c6a84261dfafe5d5e684
KirkADuran <kirkaduran@gmail.com>
2025-05-20 20:01:48 -0700

    Update lab9_p2.py

--- lab9_p2.py
+++ lab9_p2.py
@@ -1,16 +1,4 @@
 def bfs(graph, start):
-    visited = []
-    queue = [start]
 
-    while queue:
-        current = queue.pop(0)
-
-        if current not in visited:
-            visited.append(current)
-
-            # Enqueue neighbors in sorted numerical order
-            for neighbor in sorted(graph[current]):
-                if neighbor not in visited and neighbor not in queue:
-                    queue.append(neighbor)
 
     return visited
